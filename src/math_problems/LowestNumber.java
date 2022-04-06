b6bd44d24610d628ff5b1ebbdfbe6ab5bde043db
Hammad004 <go2ahammad@hotmail.com>
2022-04-06 17:55:28 -0400

    6th commit

--- src/math_problems/LowestNumber.java
+++ src/math_problems/LowestNumber.java
@@ -10,8 +10,15 @@ public class LowestNumber {
 
         int[] array = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
                 67, 5, 679, 54, 32, 65};
-
-
+int smallest = Integer.MAX_VALUE;
+int index = 0;
+while (index < array.length) {
+    if (smallest > array[index]) {
+    smallest = array[index];
+    }
+    index++;
+}
+        System.out.println("The lowest number in the array is " + smallest);
     }
 
 }
