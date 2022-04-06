588eeaec63e55680ab9203ada22fa9a52b18fcd0
Hammad004 <go2ahammad@hotmail.com>
2022-04-05 23:26:15 -0400

    3rd commit

--- src/math_problems/Factorial.java
+++ src/math_problems/Factorial.java
@@ -8,9 +8,16 @@ public class Factorial {
      *
      * HINT: Factorial of 5! = 5 x 4 X 3 X 2 X 1 = 120
      */
-
+static int factorial(int n) {
+    if (n != 0)
+        return n*factorial(n-1);
+    else
+        return 1;
+}
     public static void main(String[] args) {
-
+int number = 6, result;
+result = factorial(number);
+        System.out.println(number + " factorial = " + result);
 
     }
 
