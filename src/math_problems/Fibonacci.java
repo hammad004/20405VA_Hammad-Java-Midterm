ac0928ee115e76f4c0d33e1e3a61a244f09fcbf6
Hammad004 <go2ahammad@hotmail.com>
2022-04-06 15:36:58 -0400

    4th commit

--- src/math_problems/Fibonacci.java
+++ src/math_problems/Fibonacci.java
@@ -8,8 +8,20 @@ public class Fibonacci {
      * e.g. - 0,1,1,2,3,5,8,13
      **/
 
-    public static void main(String[] args) {
-
+       static void Fibonacci(int n) {
+    int num1 = 0, num2 = 1;
+    int counter = 0;
+    while (counter < n) {
+        System.out.println(num1 + " ");
+        int num3 = num1 + num2;
+        num1 = num2;
+        num2 = num3;
+        counter = counter + 1;
+    }
+        }
+        public static void main(String[] args) {
+           int n = 40;
+           Fibonacci(n);
     }
 
 }
