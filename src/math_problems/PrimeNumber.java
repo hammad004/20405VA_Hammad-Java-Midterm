315b81a421c2fe59b08ca49fdee79bc64d04936a
Hammad004 <go2ahammad@hotmail.com>
2022-04-06 18:08:30 -0400

    8th commit

--- src/math_problems/PrimeNumber.java
+++ src/math_problems/PrimeNumber.java
@@ -7,11 +7,30 @@ public class PrimeNumber {
      *  Print out the prime numbers in the given range.
      *
      * BONUS: Figure out how to improve algorithmic efficiency
+     *
      */
 
 
-    public static void main(String[] args) {
 
+static boolean isPrime(int n) {
+    if(n<=1)
+        return false;
+    for (int i =2; i < n; i++)
+        if (n % i ==0)
+            return false;
+    return true;
+        }
+static void printPrime(int n) {
+    for (int i = 2; i<=n; i++) {
+        if (isPrime(i))
+            System.out.println(i + " ");
+    }
+        }
+
+    public static void main(String[] args) {
+        int n = 1000000;
+        printPrime(n);
+    }
     }
 
-}
+
