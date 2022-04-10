556fc03d1df710cd7bb74a66d76bc5db74538990
Hammad004 <go2ahammad@hotmail.com>
2022-04-10 04:40:45 -0400

    Final push?

--- src/sorting/SortEfficiency.java
+++ src/sorting/SortEfficiency.java
@@ -64,7 +64,21 @@ public class SortEfficiency {
         randomize(numberArray);
 
         // region Bubble Sort
-
+public static void bubbleSort(int[] a) {
+    boolean sorted = false;
+    int temp;
+    while (!sorted) {
+        sorted = true;
+        for (int i=0; i< numberArray.length - 1; i++) {
+          if (a[i] > a[i+1]) {
+              temp = a[i];
+              a[i] = a[i + 1];
+              a[i+1] = temp;
+              sorted = false;
+          }
+        }
+    }
+        }
         // endregion
 
         randomize(numberArray);
