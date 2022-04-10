03d3ad6b0efec670f3890f614c582c5a40bde325
Hammad004 <go2ahammad@hotmail.com>
2022-04-09 21:26:36 -0400

    finished permutation

--- src/string_problems/Permutation.java
+++ src/string_problems/Permutation.java
@@ -9,4 +9,21 @@ public class Permutation {
      */
 
     // Implement Here
+    static void printPermutation(String str, String ans){
+        if (str.length() == 0) {
+            System.out.println(ans + " ");
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            String ros = str.substring(0, i) + str.substring(i+1);
+            printPermutation(ros, ans + ch);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+        printPermutation(s, "");
+    }
 }
